4341ce8ae986e6778ae180c21ae70f3a45778931
Lee Raulin <leeraulin@gmail.com>
2018-04-18 19:10:11 -0400

    Its alive!

--- src/components/WaistRecord.js
+++ src/components/WaistRecord.js
@@ -1,74 +1,78 @@
 import React, { Component } from 'react';
 
 const spacer = {
-  paddingRight: '3em',
+	paddingRight: '3em',
 };
 
 const numField = {
-  width: '4em',
+	width: '4em',
 };
 
 class WaistRecord extends Component {
-  state = { edit: false, date: '', cm: '' };
+	state = { edit: false, date: '', cm: '' };
 
-  handleEdit = () => {
-    if (!this.state.edit) {
-      this.setState({ edit: true });
-    } else {
-      this.setState({ edit: false });
-    }
-  };
+	handleEdit = () => {
+		if (!this.state.edit) {
+			this.setState({ edit: true });
+		} else {
+			this.setState({ edit: false });
+		}
+	};
 
-  handleDateChange = () => {
-    console.log();
-  };
+	handleDateChange = (e) => {
+		this.setState({ date: e.target.value });
+	};
 
-  handleCmChange = () => {
-    console.log();
-  };
+	handleCmChange = (e) => {
+		this.setState({ cm: e.target.value });
+	};
 
-  componentDidMount() {
-    this.setState({ date: this.props.date });
-    this.setState({ cm: this.props.cm });
-  }
+	onSave = () => {
+		this.props.handleSubmit(this.state.cm, this.state.date);
+	};
 
-  componentWillReceiveProps() {
-    // Update state with new props
-  }
+	componentDidMount() {
+		this.setState({ date: this.props.date });
+		this.setState({ cm: this.props.cm });
+	}
 
-  render() {
-    const editButtonText = this.state.edit ? 'Save' : 'Edit';
-    const dateField = this.state.edit ? (
-      <input
-        type="date"
-        onChange={this.handleDateChange}
-        value={this.state.date}
-        size={8}
-      />
-    ) : (
-      this.state.date
-    );
-    const cmField = this.state.edit ? (
-      <input
-        style={numField}
-        type="number"
-        onChange={this.handleCmChange}
-        value={this.state.cm}
-      />
-    ) : (
-      this.state.cm
-    );
-    return (
-      <tr>
-        <td style={spacer}>{dateField}</td>
-        <td>{cmField}</td>
-        <td>
-          <button onClick={this.handleEdit}>{editButtonText}</button>
-          <button>Delete</button>
-        </td>
-      </tr>
-    );
-  }
+	componentWillReceiveProps() {
+		// Update state with new props
+	}
+
+	render() {
+		const editButtonText = this.state.edit ? 'Save' : 'Edit';
+		const dateField = this.state.edit ? (
+			<input
+				type="date"
+				onChange={this.handleDateChange}
+				value={this.state.date}
+				size={8}
+			/>
+		) : (
+			this.state.date
+		);
+		const cmField = this.state.edit ? (
+			<input
+				style={numField}
+				type="number"
+				onChange={this.handleCmChange}
+				value={this.state.cm}
+			/>
+		) : (
+			this.state.cm
+		);
+		return (
+			<tr>
+				<td style={spacer}>{dateField}</td>
+				<td>{cmField}</td>
+				<td>
+					<button onClick={this.handleEdit}>{editButtonText}</button>
+					<button>Delete</button>
+				</td>
+			</tr>
+		);
+	}
 }
 
 export default WaistRecord;
--- src/components/WeightContainer.js
+++ src/components/WeightContainer.js
@@ -6,64 +6,69 @@ import WaistRecord from './WaistRecord';
 import Test from './Test';
 
 export default class WeightContainer extends Component {
-  state = { newMeasurement: '', edit: '', testArr: [ 1, 2, 3 ] };
+	state = { newMeasurement: '', edit: '', testArr: [ 1, 2, 3 ] };
 
-  handleLogout = () => {
-    firebase.auth().signOut();
-  };
+	handleLogout = () => {
+		firebase.auth().signOut();
+	};
 
-  handleInputChange = (e) => {
-    this.setState({ newMeasurement: e.target.value });
-  };
+	handleInputChange = (e) => {
+		this.setState({ newMeasurement: e.target.value });
+	};
 
-  handleSubmit = () => {
-    this.props.onSubmit(this.state.newMeasurement);
-    this.setState({ newMeasurement: '' });
-  };
+	handleSubmit = () => {
+		this.props.onSubmit(this.state.newMeasurement);
+		this.setState({ newMeasurement: '' });
+	};
 
-  handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      this.handleSubmit();
-    }
-  };
+	handleKeyDown = (e) => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			this.handleSubmit();
+		}
+	};
 
-  render() {
-    return (
-      <div id="WeightContainer" className="inner-container">
-        <Header>
-          <button className="red" onClick={this.handleLogout}>
-            Logout
-          </button>
-        </Header>
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Measurement</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.records.map((rec) => (
-              <WaistRecord key={rec.date} date={rec.date} cm={rec.cm} />
-            ))}
-          </tbody>
-        </table>
-        <div id="chat-input">
-          <h1>Today's Weight</h1>
-          <textarea
-            placeholder="Add your current measurement..."
-            onChange={this.handleInputChange}
-            onKeyDown={this.handleKeyDown}
-            value={this.state.newMeasurement}
-          />
-          <button onClick={this.handleSubmit}>
-            <svg viewBox="0 0 24 24">
-              <path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
-            </svg>
-          </button>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div id="WeightContainer" className="inner-container">
+				<Header>
+					<button className="red" onClick={this.handleLogout}>
+						Logout
+					</button>
+				</Header>
+				<table>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>Measurement</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.props.records.map((rec) => (
+							<WaistRecord
+								key={rec.date}
+								date={rec.date}
+								cm={rec.cm}
+								handleSubmit={this.props.onSubmit}
+							/>
+						))}
+					</tbody>
+				</table>
+				<div id="chat-input">
+					<h1>Today's Weight</h1>
+					<textarea
+						placeholder="Add your current measurement..."
+						onChange={this.handleInputChange}
+						onKeyDown={this.handleKeyDown}
+						value={this.state.newMeasurement}
+					/>
+					<button onClick={this.handleSubmit}>
+						<svg viewBox="0 0 24 24">
+							<path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
+						</svg>
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
