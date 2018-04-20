129799b568877c6549699612d9024fbbdf815d86
Lee Raulin <leeraulin@gmail.com>
2018-04-20 15:58:25 -0400

    ...

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -4,32 +4,52 @@ import Header from './Header';
 import { adonisIndex } from '../helpers';
 
 export default class UserContainer extends Component {
-	state = {
-		height: '',
-		idealWaist: '',
-		idealShoulders,
-	};
+  state = {
+    height: '',
+    idealWaist: '',
+    idealShoulders: '',
+    heightEntered: false
+  };
 
-	componentDidMount() {
-		if (!height) {
-			const height = window.prompt('Enter your height in cm');
-			this.setState({ height });
-			const { idealWaist, idealShoulders } = adonisIndex(height);
-			console.log('waist ' + idealWaist);
-			this.setState({ idealWaist, idealShoulders });
-		}
-	}
+  handleSubmit = e => {
+    console.log('handle submit');
+    e.preventDefault();
+    const height = e.target.value;
+    const { idealShoulders, idealWaist } = adonisIndex();
+    this.setState({ height, idealShoulders, idealWaist, heightEntered: true });
+  };
 
-	render() {
-		return (
-			<div id="UserContainer" className="inner-container">
-				<Header>
-					<Link to="/">
-						<button className="red">Back To Chat</button>
-					</Link>
-				</Header>
-				<h1>User</h1>
-			</div>
-		);
-	}
+  handleInput = e => {
+    this.setState({ height: e.target.value });
+  };
+
+  handleKeyDown = e => {
+    // e
+  };
+
+  render() {
+    return (
+      <div id="UserContainer" className="inner-container">
+        <Header>
+          <Link to="/">
+            <button className="red">Back To Chat</button>
+          </Link>
+        </Header>
+        <h1>User</h1>
+        {!this.state.heightEntered ? (
+          <div id="enterHeight">
+            <p>Enter your height in cm.</p>
+            <input
+              type="number"
+              onInput={this.handleInput}
+              onKeyDown={this.handleKeyDown}
+            />
+            <button type="submit" onClick={this.handleSubmit}>
+              OK
+            </button>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
--- src/components/WeightContainer.js
+++ src/components/WeightContainer.js
@@ -1,134 +1,130 @@
-import React, { Component } from 'react';
-import { Link, Route, withRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import Header from './Header';
-import WaistRecord from './WaistRecord';
-import Test from './Test';
-import { inToCm } from '../helpers';
+import React, { Component } from "react";
+import { Link, Route, withRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import WaistRecord from "./WaistRecord";
+import Test from "./Test";
+import { inToCm } from "../helpers";
 
 const btnUnit = {
-	float: 'right',
-	marginTop: '5px',
+  float: "right",
+  marginTop: "5px"
 };
 
 const btnSpacer = {
-	...btnUnit,
-	marginRight: '4em',
+  ...btnUnit,
+  marginRight: "4em"
 };
 
 class WeightContainer extends Component {
-	state = { newMeasurement: '', edit: '', testArr: [ 1, 2, 3 ], metric: true };
+  state = { newMeasurement: "", edit: "", testArr: [1, 2, 3], metric: true };
 
-	handleLogout = () => {
-		firebase.auth().signOut();
-	};
+  handleLogout = () => {
+    firebase.auth().signOut();
+  };
 
-	handleInputChange = (e) => {
-		this.setState({ newMeasurement: e.target.value });
-	};
+  handleInputChange = e => {
+    this.setState({ newMeasurement: e.target.value });
+  };
 
-	handleSubmit = () => {
-		const measurement = this.state.metric
-			? this.state.newMeasurement
-			: Math.round(inToCm(this.state.newMeasurement));
-		this.props.handleSaveRecord(measurement);
-		this.setState({ newMeasurement: '' });
-	};
+  handleSubmit = () => {
+    const measurement = this.state.metric
+      ? this.state.newMeasurement
+      : Math.round(inToCm(this.state.newMeasurement));
+    this.props.handleSaveRecord(measurement);
+    this.setState({ newMeasurement: "" });
+  };
 
-	handleKeyDown = (e) => {
-		if (e.key === 'Enter') {
-			e.preventDefault();
-			this.handleSubmit();
-		}
-	};
+  handleKeyDown = e => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      this.handleSubmit();
+    }
+  };
 
-	handleClickCm = () => {
-		this.setState({ metric: true });
-	};
+  handleClickCm = () => {
+    this.setState({ metric: true });
+  };
 
-	handleClickIn = () => {
-		this.setState({ metric: false });
-	};
+  handleClickIn = () => {
+    this.setState({ metric: false });
+  };
 
-	handleClickProfile = () => {
-		this.props.history.push(`/users/${this.props.user.uid}`);
-	};
+  handleClickProfile = () => {
+    this.props.history.push(`/users/${this.props.userID.uid}`);
+  };
 
-	render() {
-		return (
-			<div id="WeightContainer" className="inner-container">
-				<Header>
-					<button className="red" onClick={this.handleLogout}>
-						Logout
-					</button>
-					<button className="red" onClick={this.handleClickProfile}>
-						Profile
-					</button>
-					<button
-						className={
-							this.state.metric ? (
-								'btn btn-primary btn-sm active'
-							) : (
-								'btn btn-secondary btn-sm'
-							)
-						}
-						aria-pressed={this.state.metric}
-						onClick={this.handleClickCm}
-						style={btnSpacer}
-					>
-						cm
-					</button>
-					<button
-						className={
-							!this.state.metric ? (
-								'btn btn-primary btn-sm active'
-							) : (
-								'btn btn-secondary btn-sm'
-							)
-						}
-						aria-pressed={!this.state.metric}
-						onClick={this.handleClickIn}
-						style={btnUnit}
-					>
-						in
-					</button>
-				</Header>
-				<table>
-					<thead>
-						<tr>
-							<th>Date</th>
-							<th>Measurement</th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.props.records.map((rec) => (
-							<WaistRecord
-								key={rec.date}
-								date={rec.date}
-								cm={rec.cm}
-								handleSaveRecord={this.props.handleSaveRecord}
-								metric={this.state.metric}
-							/>
-						))}
-					</tbody>
-				</table>
-				<div id="chat-input">
-					<h1>Today's Weight</h1>
-					<textarea
-						placeholder="Add your current measurement..."
-						onChange={this.handleInputChange}
-						onKeyDown={this.handleKeyDown}
-						value={this.state.newMeasurement}
-					/>
-					<button onClick={this.handleSubmit}>
-						<svg viewBox="0 0 24 24">
-							<path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
-						</svg>
-					</button>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="WeightContainer" className="inner-container">
+        <Header>
+          <button className="red" onClick={this.handleLogout}>
+            Logout
+          </button>
+          <button className="red" onClick={this.handleClickProfile}>
+            Profile
+          </button>
+          <button
+            className={
+              this.state.metric
+                ? "btn btn-primary btn-sm active"
+                : "btn btn-secondary btn-sm"
+            }
+            aria-pressed={this.state.metric}
+            onClick={this.handleClickCm}
+            style={btnSpacer}
+          >
+            cm
+          </button>
+          <button
+            className={
+              !this.state.metric
+                ? "btn btn-primary btn-sm active"
+                : "btn btn-secondary btn-sm"
+            }
+            aria-pressed={!this.state.metric}
+            onClick={this.handleClickIn}
+            style={btnUnit}
+          >
+            in
+          </button>
+        </Header>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Measurement</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.records.map(rec => (
+              <WaistRecord
+                key={rec.date}
+                date={rec.date}
+                cm={rec.cm}
+                handleSaveRecord={this.props.handleSaveRecord}
+                metric={this.state.metric}
+              />
+            ))}
+          </tbody>
+        </table>
+        <div id="chat-input">
+          <h1>Today's Weight</h1>
+          <textarea
+            placeholder="Add your current measurement..."
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleKeyDown}
+            value={this.state.newMeasurement}
+          />
+          <button onClick={this.handleSubmit}>
+            <svg viewBox="0 0 24 24">
+              <path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(WeightContainer);
